2f8cb18aef59d554cebffc8ccfcb7ebd86269756
j.kasznia <j.kasznia@o2.pl>
2019-02-07 19:19:27 +0100

    aaa

--- DisposableObjects/Program.cs
+++ DisposableObjects/Program.cs
@@ -10,6 +10,24 @@ namespace DisposableObjects
     {
         static void Main(string[] args)
         {
+            TxtRepo repo = new TxtRepo();
+            try
+            {
+                Console.WriteLine(repo.GetTextFromFile("Files/TextFile.txt"));
+            }
+            finally
+            {
+                repo.Dispose();     // zniszczenie obiektu
+            }
+
+            // lub (to samo)
+            using (TxtRepo repo2 = new TxtRepo())
+            {
+                Console.WriteLine("In using");
+                Console.WriteLine(repo2.GetTextFromFile("Files/TextFile.txt"));
+            }
+
+            Console.Read();
         }
     }
 }
